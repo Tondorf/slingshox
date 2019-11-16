62e08ccbc463048247cce28af21d07d68038b7bc
Nis Meinert <mail@nismeinert.de>
2019-11-16 21:33:31 +0100

    mark variables private

--- coordinator/server.py
+++ coordinator/server.py
@@ -9,10 +9,10 @@ from tcpserver import TCPServer
 class GameServer(TCPServer):
     def __init__(self, host, port, fps):
         super(GameServer, self).__init__(host, port, fps)
-        self.players = set()
+        self._players = set()
 
         self._forces = None
-        self.world = {
+        self._world = {
             'earth': {
                 'x': .8,
                 'y': .5,
@@ -33,33 +33,33 @@ class GameServer(TCPServer):
 
     def _on_new_client(self, client_id):
         print(f'New client: {client_id}')
-        self.players.add(client_id)
+        self._players.add(client_id)
 
     def _on_leave_client(self, client_id):
         print(f'Client has left: {client_id}')
-        self.players.add(client_id)
+        self._players.add(client_id)
 
     def _on_client_message(self, message, client_id):
         print(f'Received from client {client_id}', message)
-        self.players.remove(client_id)
+        self._players.remove(client_id)
 
     def _update_world(self):
         planets = ['earth', 'moon']
-        xs = [np.array([self.world[p]['x'], self.world[p]['y']]) for p in planets]
-        vs = [np.array([self.world[p]['vx'], self.world[p]['vy']]) for p in planets]
-        ms = [self.world[p]['m'] for p in ['earth', 'moon']]
+        xs = [np.array([self._world[p]['x'], self._world[p]['y']]) for p in planets]
+        vs = [np.array([self._world[p]['vx'], self._world[p]['vy']]) for p in planets]
+        ms = [self._world[p]['m'] for p in ['earth', 'moon']]
         dt = 1
         xs, ys, self._forces, ms, dt = physics.integrate(xs, vs, self._forces, ms, dt)
 
         for i, p in enumerate(planets):
             x, y = xs[i]
-            self.world[p]['x'] = x
-            self.world[p]['y'] = y
+            self._world[p]['x'] = x
+            self._world[p]['y'] = y
 
             vx, vy = vs[i]
-            self.world[p]['vx'] = vx
-            self.world[p]['vy'] = vy
+            self._world[p]['vx'] = vx
+            self._world[p]['vy'] = vy
 
     def _generate_broadcast_messages(self):
         self._update_world()
-        return [json.dumps(self.world), ]
+        return [json.dumps(self._world), ]
