9aefc7d22c76ccc8b4b40a35921f5bd0d55cf95a
Nis Meinert <mail@nismeinert.de>
2019-11-16 22:02:35 +0100

    add do_stuff loop

--- visualizer/test_tcpclient.py
+++ visualizer/test_tcpclient.py
@@ -4,22 +4,42 @@
 import asyncio
 
 
-async def handle(loop):
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888, loop=loop)
+class TCPClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
-    running = True
-    while running:
-        data = await reader.readline()
-        if data:
-            print('Received:', data.decode().rstrip())
-        else:
-            running = False
+    async def start(self, loop):
+        reader, writer = await asyncio.open_connection(self.host, self.port, loop=loop)
 
-    print('Close the socket')
-    writer.close()
+        running = True
+        while running:
+            data = await reader.readline()
+            if data:
+                print('Received:', data.decode().rstrip())
+            else:
+                running = False
+
+        print('Close the socket')
+        writer.close()
+
+
+async def do_stuff(loop):
+    while True:
+        print('doing stuff...')
+        await asyncio.sleep(2.5, loop=loop)
+
+
+async def start(loop):
+    client = TCPClient('127.0.0.1', 8888)
+    tasks = [client.start(loop), do_stuff(loop)]
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED, loop=loop)
+
+    for task in pending:
+        task.cancel()
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(handle(loop))
+    loop.run_until_complete(start(loop))
     loop.close()
