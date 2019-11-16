dad90c63af0e42345697ce05c72995dcc9954368
Lennart Buhl <l.buhl@tarent.de>
2019-11-17 00:33:38 +0100

    connect from anywhere

--- coordinator/main.py
+++ coordinator/main.py
@@ -4,5 +4,5 @@ from server import GameServer
 
 if __name__ == '__main__':
     fps = 10.
-    server = GameServer('127.0.0.1', 8888, fps=fps)
+    server = GameServer('0.0.0.0', 8888, fps=fps)
     server.start()
