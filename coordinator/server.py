73c7527e1ebbc04b5e0a0d6238b8eb6f3aaf705a
Nis Meinert <mail@nismeinert.de>
2019-11-17 00:40:10 +0100

    more future for trajectories

--- coordinator/server.py
+++ coordinator/server.py
@@ -65,7 +65,7 @@ class GameServer(TCPServer):
 
         fs = self._forces
         for i in range(1000):
-            xs, vs, fs, _, _ = physics.integrate(xs, vs, fs, ms, dt)
+            xs, vs, fs, _, _ = physics.integrate(xs, vs, fs, ms, 50 * dt)
             if i % 10 == 0:
                 txs.append(xs)
 
