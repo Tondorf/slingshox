747e77767ad5e54bd280c4b3fc4be24df957034c
Lennart Buhl <l.buhl@tarent.de>
2019-11-17 02:12:19 +0100

    thrust via joystick

--- visualizer/definitions.py
+++ visualizer/definitions.py
@@ -28,3 +28,18 @@ PLAYER_4_KEYS = {K_KP4: EvAct.Left, K_KP6: EvAct.Right, K_4: EvAct.Left, K_6: Ev
 CONTROL_KEYS = MENU_KEYS.keys() | INGAME_KEYS.keys()
 MOVEMENT_KEYS = PLAYER_1_KEYS.keys() | PLAYER_2_KEYS.keys() | PLAYER_3_KEYS.keys() | PLAYER_4_KEYS.keys()
 KEYS = CONTROL_KEYS | MOVEMENT_KEYS
+
+# SNES-like joystick
+#   Cross X=0: links=-1 rechts=1
+#   Cross Y=1: oben=-1 unten=1
+#   Buttons: A=1 B=2 X=0 Y=3   L=4 R=5   Select=8 Start=9
+SNES_AXIS_X = 0
+SNES_AXIS_Y = 1
+SNES_A = 1
+SNES_B = 2
+SNES_X = 0
+SNEX_Y = 3
+SNES_L = 4
+SNES_R = 5
+SNES_Select = 8
+SNEX_Start = 9
