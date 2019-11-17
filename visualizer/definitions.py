2c3b33ef4b4d0cb9ebd8436f9fd9f77743ba1f15
Lennart Buhl <l.buhl@tarent.de>
2019-11-17 02:01:33 +0100

    thrust

--- visualizer/definitions.py
+++ visualizer/definitions.py
@@ -19,7 +19,7 @@ EvAct = enum('Up', 'Down', 'Left', 'Right', 'Submit', 'Exit', 'Pause')
 # variable initiations
 MENU_KEYS = {K_LEFT: EvAct.Left, K_RIGHT: EvAct.Right, K_DOWN: EvAct.Down, K_UP: EvAct.Up, K_RETURN: EvAct.Submit, K_SPACE: EvAct.Submit, K_ESCAPE: EvAct.Exit}
 INGAME_KEYS = {K_ESCAPE: EvAct.Exit, K_PAUSE: EvAct.Pause}
-PLAYER_1_KEYS = {K_LEFT: EvAct.Left, K_RIGHT: EvAct.Right}
+PLAYER_1_KEYS = {K_LEFT: EvAct.Left, K_RIGHT: EvAct.Right, K_SPACE: EvAct.Submit}
 PLAYER_2_KEYS = {K_z: EvAct.Left, K_y: EvAct.Left, K_x: EvAct.Right} # y is treated like z because Z and Y are swapped on German keyboards
 PLAYER_3_KEYS = {K_n: EvAct.Left, K_m: EvAct.Right}
 PLAYER_4_KEYS = {K_KP4: EvAct.Left, K_KP6: EvAct.Right, K_4: EvAct.Left, K_6: EvAct.Right}  # also include 4 and 6 so that brush 4 can be controlled on keyboards without a numpad
